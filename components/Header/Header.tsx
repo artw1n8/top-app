fed90bb693894bc2ca91eb224a7899765653f866
art tem <kalinichenkoartemiywork@gmail.com>
2026-02-28 10:23:29 +0300

    Header was created

---
+++ components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+import { HeaderProps } from './Header.props';
+
+export const Header = ({...props} : HeaderProps) => {
+
+	return (
+		<div {...props}>
+			Header
+		</div>
+	);
+};
