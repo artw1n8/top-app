efce162a233f3b49d86a72d11c2de25513e58eab
art tem <kalinichenkoartemiywork@gmail.com>
2026-05-02 20:04:49 +0300

    next config was changed

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // output: 'export', 
-  basePath: '/top-app',
+  // basePath: '/top-app',
   turbopack: {
     rules: {
       '*.svg': {
